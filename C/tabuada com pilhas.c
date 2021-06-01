3577c191897f04ef6abcadc4a09a1e90c753a995
ldvssr <83244862+ldvssr@users.noreply.github.com>
2021-06-01 16:43:35 +0100

    update

--- C/tabuada com pilhas.c
+++ C/tabuada com pilhas.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdio.h>
-#include <stdlib.h>
 /*Variáveis globais do programa*/
 int pilha[10];
 int pos;
@@ -66,14 +64,14 @@ int main ()
 	{
 		/*Menu do programa*/
 		int menu;
-		printf("\n\t1 - Tabuada\n\t2 - Inserir Valor na Pilha\n\t3 - Remover Valor da Pilha\n\t4 - Apresentar o Tamanho de Valores na Pilha\n\t5 - Apresentar o Valor no Topo da Pilha\n\t6 - Verificar se a Pilha se Encontra Vazia\n\t7 - Sair\n");
+		printf("\n\t1 - Criar Tabuada\n\t2 - Remover Tabuada\n\t3 - Inserir Valor na Pilha\n\t4 - Remover Valor da Pilha\n\t5 - Apresentar o Tamanho de Valores na Pilha\n\t6 - Apresentar o Valor no Topo da Pilha\n\t7 - Verificar se a Pilha se Encontra Vazia\n\t8 - Sair\n");
 		printf("\n Qual a opcao:\t");
 		scanf("%d",&menu);
 		printf("\n");
 		switch (menu)
 		{
 			case 1:
-				/*Chamar o Procedimento CreateS*/
+				/*Criar a tabuada*/
 				printf("\n Insira um numero (entre 1 e 10):\n");
 				printf("\n ");
 				scanf("%d", &i);				
@@ -85,6 +83,13 @@ int main ()
 				}		
 				break;
 			case 2:
+				/*Remove a tabuada*/								
+				for (j=1; j<=10 ; j=j+1)
+				{						
+					printf("\n [Pop]\tO valor apresentado foi removido do topo da pilha:\t %d\n", pop(varLer));
+				}		
+				break;
+			case 3:
 				/*Chamar o Procedimento Push*/
 				
 				printf("\n [Push]\tIntroduza o valor do topo da pilha:\t");
@@ -92,26 +97,26 @@ int main ()
 				push(varLer);
 				printf("\n");
 				break;
-			case 3:
+			case 4:
 				/*Chamar a Função Pop*/
 				printf("\n [Pop]\tO valor apresentado foi removido do topo da pilha:\t");
 				printf("%d",pop());
 				printf("\n\n");
 				break;
-			case 4:
+			case 5:
 				/*Chamar a Função Size*/
 				printf("\n [Size]\tO valor apresentado refere a dimensao da pilha:");
 				printf("\t");
 				printf("%d",size());
 				printf("\n\n");
 				break;
-			case 5:
+			case 6:
 				/*Chamar a Função Peep*/
 				printf("\n [Peep]\tO valor apresentado refere o valor no topo da pilha:\t");
 				printf("%d",stackpop());
 				printf("\n\n");
 				break;
-			case 6:
+			case 7:
 				/*Chamar a Função isEmpty*/
 				if (empty()==0)
 				{
@@ -124,7 +129,7 @@ int main ()
 					printf("\n\n");
 				}
 				break;
-			case 7:
+			case 8:
 				/*Ciclo de Repetição*/
 				printf("\n\tDeseja mesmo sair? 0 - Nao / 1 - Sim \t");
 				scanf(" %d", &repetir);
