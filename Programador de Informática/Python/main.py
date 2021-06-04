b3275434ecd1e844c021495877b077fb1848fb67
Daniel Ventura <ldvssr@icloud.com>
2021-06-04 16:18:36 +0100

    update

---
+++ Programador de Informática/Python/main.py
@@ -0,0 +1,25 @@
+# isto é um comentário
+'''isto é um comentario em bloco'''
+#ciclo for
+for n in range(10):
+    print(n, "Daniel")
+#lista com 5 elementos
+a = [5,23,"Daniel", 47,"Ventura"]
+print(a)
+#
+a.append("Manuela")
+print(a)
+#
+a.remove("Daniel")
+#
+a.pop(2)
+print(a)
+#
+t=[15,"ab","Maria"]
+a.append(t)
+print(a)
+print(a[4])
+print(a[4][2])
+x=["brilha","no","escuro"]
+t.append(x)
+print(a)
