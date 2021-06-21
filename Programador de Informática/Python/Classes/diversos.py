71812d761116b792a055acc2ed26059837985961
Daniel Ventura <83244862+ldvssr@users.noreply.github.com>
2021-06-21 16:43:48 +0100

    new

--- Programador de Informática/Python/Classes/diversos.py
+++ Programador de Informática/Python/Classes/diversos.py
@@ -1,3 +1,15 @@
+#classes
 class diversos:
-    def nome (self, name):
-        print (name)
+    #construtor
+    def __init__(self, name):        
+        self.name = name
+    #métodos/atributos        
+    def nome (self):
+        print (self.name)
+    def apelido (self):
+        print (self.name, "Ventura")
+        #pass - é usado para deixar de dar erro quando a função não tem nada lá dentro
+    def idade (self):
+        print (34)
+    def greeting (self):
+        print ("Olá")
