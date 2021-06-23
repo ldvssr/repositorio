e208f6ab17afa5d5325deebfd11232e1949e886c
Daniel Ventura <ldvssr@icloud.com>
2021-06-23 18:02:50 +0100

    update

---
+++ Programador de Informática/Python/ficheiros/main.py
@@ -0,0 +1,16 @@
+from ficheiros import *
+
+#importar o sistema operativo
+#import os
+
+fic = ficheiros ()
+
+#fic.save("Nome", "Idade", "Tamanho")
+#fic.save("Miguel", "27", "32")
+
+#fic.read()
+
+#fic.remove_line()
+
+#usar o comando para eliminar o ficheiro juntamente com o import os
+#os.remove(r"C:\repositorio\Programador de Informática\Python\ficheiros\myFile")
