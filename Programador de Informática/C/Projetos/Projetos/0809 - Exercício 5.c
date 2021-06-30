9ee10b72c44f337d853d4bac4459df6875abca3c
Daniel Ventura <83244862+ldvssr@users.noreply.github.com>
2021-06-30 23:11:10 +0100

    Update 0809 - Exercício 5.c

--- Programador de Informática/C/Projetos/Projetos/0809 - Exercício 5.c
+++ Programador de Informática/C/Projetos/Projetos/0809 - Exercício 5.c
@@ -5,49 +5,57 @@ vers: 1.0
 email: ldvssr@icloud.com
 desc: Exercicio 5
 obs:
-	-	Exercicio 5: Neste exercicio por favor crie um programa para verificar se um numero inserido é numero primo,
-e mostre uma mensagem a dizer" o numero inserido é primo", caso não seja, imprima a mensagem "o numero inserido NÃO é primo"
-Também verifique se o numero inserido é superior a 10 e mostre uma mensagem caso o seja- "o numero inserido é superior a 10"
-Também terá de fazer validação de inputs, sendo que o programa deve poder detectar que o utilizador carregou numa tecla que não é um numero
-e avisar de tal erro através de uma mensagem.
+	-	Exercicio 5: Neste exercicio por favor crie um programa para verificar se um numero inserido ï¿½ numero primo,
+e mostre uma mensagem a dizer" o numero inserido ï¿½ primo", caso nï¿½o seja, imprima a mensagem "o numero inserido Nï¿½O ï¿½ primo"
+Tambï¿½m verifique se o numero inserido ï¿½ superior a 10 e mostre uma mensagem caso o seja- "o numero inserido ï¿½ superior a 10"
+Tambï¿½m terï¿½ de fazer validaï¿½ï¿½o de inputs, sendo que o programa deve poder detectar que o utilizador carregou numa tecla que nï¿½o ï¿½ um numero
+e avisar de tal erro atravï¿½s de uma mensagem.
 */
-//Inclusão de bibliotecas.
+//Inclusï¿½o de bibliotecas.
 #include <stdio.h>
 #include <locale.h>
 //Desabilitar o aviso de erro 4996.
 #pragma warning (disable: 4996)
 //Constante.
 #define PI 3.14
-//Região Main.
+//Regiï¿½o Main.
 #pragma region Main
 //Programa principal.
 int main()
 {
 	//Permite reconhecer os caracteres portugueses.
 	setlocale(LC_ALL, "Portuguese");
-	//Introdução do programa.
-	printf("\n\tEste programa verifica se é um número primo.\n\n\t");
-	//Declaração de variáveis.
-	char a = ' ';
-	int b = 0;
+	//Introduï¿½ï¿½o do programa.
+	printf("\n\tEste programa verifica se ï¿½ um nï¿½mero primo.\n\n\t");
+	//Declaraï¿½ï¿½o de variï¿½veis.
 	//Ciclo para repetir.
 	int repetir = 0;
 	do
 	{
 		//Output.		
-		printf("Introduza um número: ");
+		printf("Introduza um nï¿½mero: ");
 		//Input.
-		//Para limpar o buffer de memória do teclado na segunda leitura adiciona-se um espaço antes de %c.
-		//scanf(" %c", &a);
-		scanf(" %d", &b);
-		//Operação.//Resposta.
-				
+		//Para limpar o buffer de memï¿½ria do teclado na segunda leitura adiciona-se um espaï¿½o antes de %c.
+		//scanf(" %c", &a);		
+		//Operaï¿½ï¿½o.//Resposta.		 
+		int loop, number;
+		int prime = 1;		
+		scanf(" %d", &number);
+		for(loop = 2; loop < number; loop++) {
+			if((number % loop) == 0) {
+				prime = 0;
+			}
+		}
+		if (prime == 1)
+			printf("%d is prime number.", number);
+		else
+			printf("%d is not a prime number.", number);
 		//printf("Foi introduzido %d cujo caracter = %c\n", b, (char) b);
 		/*printf("%c", a);
 		a = b;
 		if (a >= 48 && a <= 57)
 		{
-			printf("\n\tIntroduziu um número.\n\t");
+			printf("\n\tIntroduziu um nï¿½mero.\n\t");
 		}
 		else if (a >= 65 && a <= 90 || a >= 97 && a <= 122)
 		{
@@ -55,14 +63,14 @@ int main()
 		}
 		else
 		{
-			printf("\n\tIntroduza um número ou uma letra.\n\t");
+			printf("\n\tIntroduza um nï¿½mero ou uma letra.\n\t");
 		}*/
-		printf("\n\tDeseja continuar? 0 - Sim / 1 - Não: ");
+		printf("\n\tDeseja continuar? 0 - Sim / 1 - Nï¿½o: ");
 		scanf(" %d", &repetir);
 		printf("\n\t");
 	} while (repetir == 0);
 	//Para devolver 0 caso o programa seja executado corretamente.
 	return 0;
 }
-//Fim da Região Main
+//Fim da Regiï¿½o Main
 #pragma endregion
