d771137d5473dd291d5b85ab0eec64af260bc4dc
Daniel Ventura <ldvssr@icloud.com>
2021-07-01 12:40:28 +0100

    up

--- Programador de Informática/C/Projetos/Projetos/0809 - Exercício 5.c
+++ Programador de Informática/C/Projetos/Projetos/0809 - Exercício 5.c
@@ -5,13 +5,13 @@ vers: 1.0
 email: ldvssr@icloud.com
 desc: Exercicio 5
 obs:
-	-	Exercicio 5: Neste exercicio por favor crie um programa para verificar se um numero inserido � numero primo,
-e mostre uma mensagem a dizer" o numero inserido � primo", caso n�o seja, imprima a mensagem "o numero inserido N�O � primo"
-Tamb�m verifique se o numero inserido � superior a 10 e mostre uma mensagem caso o seja- "o numero inserido � superior a 10"
-Tamb�m ter� de fazer valida��o de inputs, sendo que o programa deve poder detectar que o utilizador carregou numa tecla que não � um numero
-e avisar de tal erro atrav�s de uma mensagem.
+	-	Exercicio 5: Neste exercicio por favor crie um programa para verificar se um numero inserido é numero primo,
+e mostre uma mensagem a dizer" o numero inserido é primo", caso não seja, imprima a mensagem "o numero inserido NÃO é primo"
+Também verifique se o numero inserido é superior a 10 e mostre uma mensagem caso o seja- "o numero inserido é superior a 10"
+Também terá de fazer validação de inputs, sendo que o programa deve poder detectar que o utilizador carregou numa tecla que não é um numero
+e avisar de tal erro através de uma mensagem.
 */
-//Inclus�o de bibliotecas.
+//Inclusão de bibliotecas.
 #include <stdio.h>
 #include <locale.h>
 //Desabilitar o aviso de erro 4996.
@@ -25,19 +25,18 @@ int main()
 {
 	//Permite reconhecer os caracteres portugueses.
 	setlocale(LC_ALL, "Portuguese");
-	//Introdu��o do programa.
-	printf("\n\tEste programa verifica se um número é primo.\n\n\t");
-	//Declara��o de vari�veis.
+	//Introduçãoo do programa.
+	printf("\n\tEste programa verifica se um numero e primo.\n\n\t");
+	//Declaração de variáveis.
 	//Ciclo para repetir.
-	int repetir = 0;
+	int repetir = 1;
 	do
 	{
 		//Output.		
-		printf("Introduza um número: ");
+		printf("Introduza um numero: ");
 		//Input.
-		//Para limpar o buffer de mem�ria do teclado na segunda leitura adiciona-se um espa�o antes de %c.
-		//scanf(" %c", &a);		
-		//Opera��o.//Resposta.		 
+		//Para limpar o buffer de memória do teclado na segunda leitura adiciona-se um espaço antes de %c ou  o comando fflush (stdin);.			
+		//Operação.//Resposta.		 
 		int ciclo, numero;
 		int primo = 1;		
 		scanf(" %d", &numero);
@@ -47,15 +46,15 @@ int main()
 			}
 		}
 		if (primo == 1)
-			printf("\n\t%d é um número primo.", numero);
+			printf("\n\t%d e um numero primo.\n", numero);
 		else
-			printf("\n\t%d não é um número primo.", numero);
+			printf("\n\t%d nao e um numero primo.\n", numero);
 		//printf("Foi introduzido %d cujo caracter = %c\n", b, (char) b);
 		/*printf("%c", a);
 		a = b;
 		if (a >= 48 && a <= 57)
 		{
-			printf("\n\tIntroduziu um n�mero.\n\t");
+			printf("\n\tIntroduziu um número.\n\t");
 		}
 		else if (a >= 65 && a <= 90 || a >= 97 && a <= 122)
 		{
@@ -63,14 +62,14 @@ int main()
 		}
 		else
 		{
-			printf("\n\tIntroduza um n�mero ou uma letra.\n\t");
+			printf("\n\tIntroduza um número ou uma letra.\n\t");
 		}*/
-		printf("\n\tDeseja continuar? 0 - Sim / 1 - Não: ");
+		printf("\n\tDeseja continuar? 1 - Sim / 0 - Nao: ");
 		scanf(" %d", &repetir);
 		printf("\n\t");
-	} while (repetir == 0);
+	} while (repetir == 1);
 	//Para devolver 0 caso o programa seja executado corretamente.
 	return 0;
 }
-//Fim da Regi�o Main
+//Fim da Região Main
 #pragma endregion
