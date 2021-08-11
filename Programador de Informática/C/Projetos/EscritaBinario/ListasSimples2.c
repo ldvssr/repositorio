d244200297e4b3fa33825ae6feb3740bfec56a0a
Daniel Ventura <83244862+ldvssr@users.noreply.github.com>
2021-08-11 19:36:45 +0100

    up

--- Programador de Informática/C/Projetos/EscritaBinario/ListasSimples2.c
+++ Programador de Informática/C/Projetos/EscritaBinario/ListasSimples2.c
@@ -7,6 +7,7 @@ Descrição: Iteração de listas simples ligadas
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 typedef struct
 {
@@ -16,12 +17,12 @@ typedef struct
 
 Node* head = NULL;
 
-void printList()
+void printList1()
 {
 	Node* ptr = head;
 	while (ptr != NULL)
 	{
-		printf("%d", ptr->data);
+		printf("\n%d", ptr->data);
 		ptr = ptr->next;
 	}
 }
@@ -34,14 +35,59 @@ void insert(int data)
 	head = ptr;
 }
 
+void insertEnd(int data)
+{
+	Node* newNode = (Node*)malloc(sizeof(Node));
+	newNode->data = data;
+	newNode->next = NULL;
+	Node* link = head;
+	if (head == NULL) head = newNode;
+	else
+	{
+		while (link->next != NULL)
+		{
+			link = link->next;
+		}
+		link->next = newNode;
+	}
+}
+
+int getCount(Node* head)
+{
+	int count = 0;
+	Node* temp = head;
+	while (temp!=NULL)
+	{
+		count++;
+		temp = temp->next;
+	}
+	return count;
+}
+
+bool search(int y)
+{
+	Node* current = head;
+	while (current != NULL)
+	{
+		if (current->data == y) return true;
+		current = current->next;
+	}
+	return false;
+}
+
 int main()
 {
-	insert(10);
-	insert(20);
-	insert(39);
-	insert(42);
+	insertEnd(10);
+	insertEnd(20);
+	insertEnd(30);
+	insertEnd(40);
+	insertEnd(50);
 
-	printList();
+	printf("\n%s", search(30) ? "true" : "false");
+
+	printf("\n%d elementos", getCount(head));
+
+	printList1();
 
 	getche();
 	return 0;
