06790b083108590c8f8c8f381c89bdc71c833a4c
Daniel Ventura <83244862+ldvssr@users.noreply.github.com>
2021-11-16 20:15:10 +0000

    up

---
+++ Programador de Informática/C#/ProjetoIgor/ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿/*
+ Criar código para realizar uma conexão a uma base de dados localhost MYSQL.  
+ */
+
+using MySql.Data.MySqlClient;
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //connection string
+            string connStr = "server=localhost;user=root;database=exercicio_3_-_prático;port=3306;password=batatas12345";
+
+            MySqlConnection conn = new MySqlConnection(connStr);
+
+            try
+            {
+                Console.WriteLine("A conectar a base de dados");
+                conn.Open(); //abrir a conexao a base de dados
+
+                string sql = "SELECT * FROM produtos";
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                while(reader.Read())
+                {
+                    Console.WriteLine(reader[0] + " - " + reader[1] + " - " + reader[2]);
+                }
+                reader.Close();
+            }
+            catch (Exception err)
+            { 
+                Console.WriteLine(err); 
+            }
+            conn.Close();
+        }
+    }
+}
