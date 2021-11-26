31aca927f426070dc769e692addb622708b21fbc
Daniel Ventura <83244862+ldvssr@users.noreply.github.com>
2021-11-26 18:05:11 +0000

    up

--- Programador de Informática/C#/ProjetoIgor/GestaoStock/MainGestaoStock.cs
+++ Programador de Informática/C#/ProjetoIgor/GestaoStock/MainGestaoStock.cs
@@ -1,11 +1,275 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace GestaoSock
 {
     class MainGestaoStock
     {
         static void Main(string[] args)
         {
+                                   
+            List<Produto> listaProdutos = new List<Produto>();
+
+            for (int i = 0; i < 3; i++)
+            {
+
+                Produto produto = criarProduto();
+
+                listaProdutos.Add(produto);
+
+            }                      
+
+            ImprimirLista(listaProdutos);
+
+            Console.WriteLine("Indique um id de produto para invalidar: ");
+
+            String codigoIDProduto = Console.ReadLine();
+
+            InvalidarProduto(codigoIDProduto, listaProdutos);
+
+            ImprimirLista(listaProdutos);
+
+
+
+            Console.WriteLine("Indique um id de produto para alterar o stock: ");
+
+            String codigoID = Console.ReadLine();
+
+            float novaQuantidade;
+
+            float.TryParse(Console.ReadLine(), out novaQuantidade);
+
+            AlterarStock(codigoID, listaProdutos, novaQuantidade);
+
+            ImprimirLista(listaProdutos);                        
 
         }
+
+        static Produto criarProduto()
+        {
+
+            Console.WriteLine("Indique nome: "); 
+            String nome = Console.ReadLine();
+
+            Console.WriteLine("Indique valor: ");
+            float valor;
+            float.TryParse(Console.ReadLine(), out valor);
+
+            Console.WriteLine("Indique quantidade: ");
+            float quantidade;
+            float.TryParse(Console.ReadLine(), out quantidade);
+
+            Console.WriteLine("Indique iva: ");
+            int iva;
+            int.TryParse(Console.ReadLine(), out iva);
+
+            Console.WriteLine("Indique codigoID ");
+            String codigoID = Console.ReadLine();
+
+            Console.WriteLine("Indique margemDeLucro: ");
+            float margemDeLucro;
+            float.TryParse(Console.ReadLine(), out margemDeLucro);                                 
+            bool isInvalido = false;
+
+            Produto produto = new Produto(nome, valor, quantidade, iva, codigoID, margemDeLucro, isInvalido);
+
+            return produto;
+
+        }
+
+        static void ImprimirLista(List<Produto>listaProdutos)
+        {
+
+            foreach (Produto prod in listaProdutos)
+            {
+
+                Console.WriteLine(prod);
+
+            }
+
+        }
+        
+        static void InvalidarProduto (String idCodigo, List<Produto> listaProdutos)
+        {
+
+            //iterar pela lista
+            //encontrar o codigo de id de cada produto e verificar se corresponde ao enviado por parametro
+            //modificar o valor do bool
+
+            foreach (Produto prod in listaProdutos)
+            {
+
+                if (prod.codigoID == idCodigo)
+                {
+
+                    prod.isInvalido = true;
+
+                }
+
+            }
+
+        }
+        static void AlterarStock(String idCodigo, List<Produto> listaProdutos, float novaQuantidade)
+        {
+
+            //Iterar pela lista
+            //encontrar o codigo de id de cada produto e verificar se corresponde ao enviado por parametro
+            //modificar o valor do bool;
+            foreach (Produto prod in listaProdutos)
+            {
+                if (prod.codigoID == idCodigo)
+                {
+                    prod.quantidade = novaQuantidade;
+                }
+            }
+        }        
+
+    }
+
+}
+
+/*
+
+using System;
+using System.Collections.Generic;
+
+namespace GestaoStock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                List<Produto> listaProdutos = new List<Produto>();
+                for (int i = 0; i < 3; i++)
+                {
+                Produto produto = criarProduto();
+                listaProdutos.Add(produto);
+
+                }
+              
+                ImprimirLista(listaProdutos);
+
+
+            Console.WriteLine("Indique um id de produto para invalidar");
+            String codigoIDProduto = Console.ReadLine();
+            InvalidarProduto(codigoIDProduto, listaProdutos);
+            ImprimirLista(listaProdutos);
+
+
+            Console.WriteLine("Indique um id de produto para invalidar");
+            String codigoID = Console.ReadLine();
+            float novaQuantidade;
+            float.TryParse(Console.ReadLine(), out novaQuantidade);
+            AlterarStock(codigoID, listaProdutos,novaQuantidade);
+
+            ImprimirLista(listaProdutos);
+       
+
+        //Função 
+
+
+
+
+        //TODO
+        //Fazer Classe
+        //Produto - Nome, Preço, Quantidade , IVA, códigoID, margemDeLucro, isInvalido;
+
+
+        //Loja com lista de Produtos
+
+        //Lista de Compras 
+
+        //Poder aceder à lista de produtos
+        //Poder adicionar um produto
+        //Poder invalidar um produto
+        //Poder alterar numero De Stock
+
+
+        //Função Procurar um codigoID, na lista de produtos,
+
+
+
+    }
+
+        static Produto criarProduto()
+        {
+            Console.WriteLine("Indique nome:");
+            String nome = Console.ReadLine();
+
+            Console.WriteLine("Indique valor");
+            float valor;
+            float.TryParse(Console.ReadLine(), out valor);
+
+            Console.WriteLine("Indique Quantidade");
+            float quantidade;
+            float.TryParse(Console.ReadLine(), out quantidade);
+
+            Console.WriteLine("Indique Iva ");
+            int iva;
+            int.TryParse(Console.ReadLine(), out iva);
+
+
+            Console.WriteLine("Indique codigoId: ");
+            String codigoID = Console.ReadLine();
+           
+
+            Console.WriteLine("Indique margem Lucro: ");
+            float margemDeLucro;
+            float.TryParse(Console.ReadLine(), out margemDeLucro);
+
+            bool isInvalido = false;
+            Produto produto = new Produto(nome, valor, quantidade, iva, codigoID, margemDeLucro, isInvalido);
+
+            return produto;
+
+        }
+
+
+        static void ImprimirLista(List<Produto> listaProdutos)
+        {
+
+            foreach(Produto prod in listaProdutos) {
+
+                Console.WriteLine(prod);
+            }
+
+
+        }
+
+
+
+        static void InvalidarProduto(String idCodigo, List<Produto> listaProdutos)
+        {
+
+            //Iterar pela lista
+            //encontrar o codigo de id de cada produto e verificar se corresponde ao enviado por parametro
+            //modificar o valor do bool;
+            foreach (Produto prod in listaProdutos)
+            {
+                if (prod.codigoID == idCodigo)
+                {
+                    prod.isInvalido = true;
+                }
+            }
+        }
+
+        static void AlterarStock(String idCodigo, List<Produto> listaProdutos, float novaQuantidade)
+        {
+
+            //Iterar pela lista
+            //encontrar o codigo de id de cada produto e verificar se corresponde ao enviado por parametro
+            //modificar o valor do bool;
+            foreach (Produto prod in listaProdutos)
+            {
+                if (prod.codigoID == idCodigo)
+                {
+                    prod.quantidade = novaQuantidade;
+                }
+            }
+        }
+
+
     }
 }
+
+*/
--- Programador de Informática/C#/ProjetoIgor/GestaoStock/Produto.cs
+++ Programador de Informática/C#/ProjetoIgor/GestaoStock/Produto.cs
@@ -1,12 +1,125 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
 namespace GestaoSock
 {
     class Produto
     {
-        //Construtor
+        //ESTADO
+        public string nome;
+        public float valor;
+        public float quantidade;
+        public int iva;
+        public string codigoID;            
+        public float valorTotal;
+        public bool isInvalido;
+        //Construtor Padrão
         public Produto()
         {
 
         }
+        //Construtores Parametrizados
+        public Produto(String nome, float valor, float quantidade, int iva, String codigoID, float margemDeLucro, bool isInvalid)
+        {
+
+            this.nome = nome;
+            this.valor = valor;
+            this.quantidade = quantidade;
+            this.iva = iva;
+            this.codigoID = codigoID;
+            this.valorTotal = quantidade * valor;
+            this.isInvalido = isInvalid;
+
+        }
+
+        //Overload de Construtores
+        public Produto(string nome, float valor, float quantidade, int iva, string codigoID, float margemDeLucro)
+        {
+
+            this.nome = nome;
+            this.valor = valor;
+            this.quantidade = quantidade;
+            this.iva = iva;
+            this.codigoID = codigoID;
+            this.valorTotal = quantidade * valor;
+            this.isInvalido = false;
+
+        }
+
+        public override string ToString()
+        {
+            
+            return this.nome + " : " + this.valor + " | quantidade: " + this.quantidade +" INVALIDO? " + this.isInvalido;            
+
+        }
+
     }
 }
+
+/*
+ *
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GestaoStock
+{
+    class Produto
+    {
+
+         //ESTADO
+         public String nome;
+         public float valor;
+         public float quantidade;
+         public int iva;
+         public String codigoID;
+         public float margemDeLucro;
+         public float valorTotal;
+         public bool isInvalido;
+
+        //TODO
+        //Fazer Classe
+        //Produto - Nome, Preço, Quantidade , IVA, códigoID, margemDeLucro, isInvalido;
+
+        //Construtor Padrão
+        public Produto()
+        {
+
+        }
+
+        //Construtores Parametrizados
+
+        public Produto(String nome, float valor, float quantidade, int iva, String codigoID, float margemDeLucro,bool isInvalid)
+        {
+            this.nome = nome;
+            this.valor = valor;
+            this.quantidade = quantidade;
+            this.iva = iva;
+            this.codigoID = codigoID;
+            this.valorTotal = quantidade * valor;
+            this.isInvalido = isInvalid;
+
+        }
+
+        //overload de Construtores
+        public Produto(String nome, float valor, float quantidade, int iva, String codigoID, float margemDeLucro)
+        {
+            this.nome = nome;
+            this.valor = valor;
+            this.quantidade = quantidade;
+            this.iva = iva;
+            this.codigoID = codigoID;
+            this.valorTotal = quantidade * valor;
+            this.isInvalido = false;
+
+        }
+
+        public override string ToString()
+        {
+            return this.nome+ ": " + this.valor + " | quantidade: " + this.quantidade + " INVALIDO? " + this.isInvalido;
+        }
+    }
+}
+*
+*/
