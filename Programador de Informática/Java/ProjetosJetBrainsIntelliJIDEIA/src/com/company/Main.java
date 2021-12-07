8991cf0615f7644730da4db90befeabd07830e7c
Daniel Ventura <83244862+ldvssr@users.noreply.github.com>
2021-12-07 23:01:28 +0000

    up

--- Programador de Informática/Java/ProjetosJetBrainsIntelliJIDEIA/src/com/company/Main.java
+++ Programador de Informática/Java/ProjetosJetBrainsIntelliJIDEIA/src/com/company/Main.java
@@ -1,8 +1,20 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.println("Hello World!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\""+sc.nextLine()+"\"");
+        int a = 0;
+        if (a == 0)
+        {
+            System.out.println(a);
+        }
+        for ( int i = 1; i < 4; i++)
+            System.out.println(i);
     }
 }
