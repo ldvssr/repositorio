5790372c5b381b84d2c71f26b2f4b1406af5109b
Daniel Ventura <ldvssr@icloud.com>
2022-12-22 19:42:59 +0000

    v1.11

--- CETTPSI/Visual Studio/UFCD5412Exercicio1/Balao/Balao.cs
+++ CETTPSI/Visual Studio/UFCD5412Exercicio1/Balao/Balao.cs
@@ -1,17 +1,59 @@
 ﻿namespace Balao
 {
     using System;
+    using System.Xml.Linq;
+
     internal class Balao
     {
 
         #region Atributos
 
-        private string _cor;
+        private string _cor = "";
 
-        private string _direcao;
+        private string _direcao = "";
 
-        private int _altitude;
+        private int _altitude = 0;
 
+        #endregion
+
+        #region Propriedades
+
+        public string Cor
+        {
+            get
+            {
+                return _cor;
+            }
+            set
+            {
+                _cor = value;
+            }
+        }
+
+        public string Direcao
+        {
+            get
+            {
+                return _direcao;
+            }
+            set
+            {
+                _direcao = value;
+            }
+        }
+
+        public int Altitude
+        {
+            get
+            {
+                return _altitude;
+            }
+            set
+            {
+                _altitude = value;
+            }
+        }
+       
         #endregion
 
         #region Métodos
--- CETTPSI/Visual Studio/UFCD5412Exercicio1/Veiculo/Veiculo.cs
+++ CETTPSI/Visual Studio/UFCD5412Exercicio1/Veiculo/Veiculo.cs
@@ -6,7 +6,7 @@
 
         #region Atributos
 
-        private string _matricula;
+        private string _matricula = "";
 
         private int _quilometragem;
 
@@ -15,6 +15,70 @@
         private double _combustivel;
 
         private int _contador;
+
+        #endregion
+
+        #region Propriedades
+
+        public string Matricula
+        {
+            get
+            {
+                return _matricula;
+            }
+            set
+            {
+                _matricula = value;
+            }
+        }
+
+        public int Quilometragem
+        {
+            get
+            {
+                return _quilometragem;
+            }
+            set
+            {
+                _quilometragem = value;
+            }
+        }
+
+        public double Deposito
+        {
+            get
+            {
+                return _deposito;
+            }
+            set
+            {
+                _deposito = value;
+            }
+        }
+
+        public double Combustivel
+        {
+            get
+            {
+                return _combustivel;
+            }
+            set
+            {
+                _combustivel = value;
+            }
+        }
+
+        public int Contador
+        {
+            get
+            {
+                return _contador;
+            }
+            set
+            {
+                _contador = value;
+            }
+        }
 
         #endregion
 
